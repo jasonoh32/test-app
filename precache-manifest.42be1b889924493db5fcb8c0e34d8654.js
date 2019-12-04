f48a638f3b79b9873bb3e9bbb21494ca15d845ad
Jason Oh <jasonoh@dhcp-10-250-210-242.harvard.edu>
2019-12-04 14:57:13 -0500

    brought .html file to root dir

---
+++ precache-manifest.42be1b889924493db5fcb8c0e34d8654.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb92518f3a7602dca8094ab9b1178750",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c94bf695af27f5255db2",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "54448ef5e430adcc1624",
+    "url": "/static/js/2.1d5677a4.chunk.js"
+  },
+  {
+    "revision": "c94bf695af27f5255db2",
+    "url": "/static/js/main.4d72d62f.chunk.js"
+  },
+  {
+    "revision": "817cc9980cb4ca11c564",
+    "url": "/static/js/runtime-main.61ffc057.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
